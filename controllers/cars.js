064d4d1d7c8d755bbbd8a186ae4b312626453757
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-26 16:04:17 -0400

    creates new and create functionality for car

--- controllers/cars.js
+++ controllers/cars.js
@@ -14,6 +14,26 @@ function index(req, res) {
   })
 }
 
+function newCar(req, res) {
+  res.render("cars/new", {
+    title: "Add Car",
+  })
+}
+
+function create(req, res) {
+  req.body.owner = req.user.profile._id
+  Car.create(req.body)
+  .then(car => {
+    res.redirect('/cars')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/cars')
+  })
+}
+
 export {
-  index
+  index,
+  newCar as new,
+  create,
 }
--- routes/cars.js
+++ routes/cars.js
@@ -1,10 +1,17 @@
 import { Router } from 'express'
 import * as carsCtrl from '../controllers/cars.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 //GET localhost:3000/cars
 router.get('/', carsCtrl.index)
+
+//POST localhost:3000/cars/new
+router.get('/new', isLoggedIn, carsCtrl.new)
+
+//POST localhost:3000/cars
+router.post('/', carsCtrl.create)
 
 export {
   router
