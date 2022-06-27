7fe65b05c26bc4a3583c54eebb1d733396f74f4a
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-27 13:53:38 -0400

    creates schema for a subdocument in my car.js model

--- models/car.js
+++ models/car.js
@@ -2,12 +2,23 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const modSchema = new Schema({
+  name: String,
+  description: String,
+  price: String,
+  worthIt: Boolean,
+  moPower: Boolean,
+}, {
+  timestamps: true
+})
+
 const carSchema = new mongoose.Schema({
   boughtFor: String,
   year: String,
   make: String,
   model: String,
   owner: {type: Schema.Types.ObjectId, ref: "Profile"},
+  mods: [modSchema],
 }, {
   timestamps: true
 })
