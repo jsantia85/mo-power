c3b00b185f9bb90dd6145f73aa31e606eaa29c9a
jsantia85 <JuanASantiago1058@gmail.com>
2022-06-30 19:05:10 -0400

    creates show view and functionality

--- routes/profiles.js
+++ routes/profiles.js
@@ -4,7 +4,11 @@ import * as profilesCtrl from '../controllers/profiles.js'
 
 const router = Router()
 
+//GET localhost:3000/profiles
 router.get('/', isLoggedIn, profilesCtrl.index)
+
+//GET localhost:3000/profiles/:id
+router.get('/:id', isLoggedIn, profilesCtrl.show)
 
 export {
   router
