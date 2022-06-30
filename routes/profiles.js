d2c8b8bde966e90b19b48fdcbcf5914a5965cc1e
jsantia85 <JuanASantiago1058@gmail.com>
2022-06-30 19:00:00 -0400

    creates profile index page and functionality

---
+++ routes/profiles.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { isLoggedIn } from '../middleware/middleware.js'
+import * as profilesCtrl from '../controllers/profiles.js'
+
+const router = Router()
+
+router.get('/', isLoggedIn, profilesCtrl.index)
+
+export {
+  router
+}
